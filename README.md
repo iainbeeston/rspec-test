2a0b66cc53f883002700e0f3772451fabd396298
Iain Beeston <iainbeeston@users.noreply.github.com>
2019-01-26 08:35:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+RSPEC TEST
+-----
+
+Proof of concept to show that rspec-matchers' include matcher cannot handle infinite ranges.
+
+To execute, run `rake` or `rspec`.
---
+++ spec/infinite_range_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'timeout'
+
+RSpec.describe 'inclusion matcher' do
+  it 'can handle inclusion on infinite ranges' do
+    expect(1..).to include(3)
+  end
+
+  it 'can handle exclusion on infinite ranges' do
+    Timeout.timeout(5) do
+      expect(1..).not_to include(0)
+    end
+  end
+end
+
+RSpec.describe 'Range.include?' do
+  it 'can handle exclusion on infinite ranges' do
+    expect((1..).include?(0)).to eq(false)
+  end
+end
